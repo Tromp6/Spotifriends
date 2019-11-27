e9aa4ce46877c16114cf6a1864985446c8b2975d
max <you@example.com>
2019-11-27 15:43:18 +0100

    session not working

--- public/first_app.ts
+++ public/first_app.ts
@@ -45,9 +45,16 @@ app.use("/",authRoutes);
 
 
 
+app.listen(app.get("port"), () => {
+  console.log(
+    "  App is running at http://localhost:%d in %s mode",
+    app.get("port"),
+    app.get("env")
+  );
+  console.log("  Press CTRL-C to stop\n");
+});
 
-
-app.listen(3000);
+//app.listen(3000);
 
 
 
